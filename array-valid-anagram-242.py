09b81fa8cf2c5ebe8cee09575af5993feec2fe86
Nishka Awasthi <97773663+NishkaAwasthi@users.noreply.github.com>
2022-05-24 02:46:34 -0400

    Create array-valid-anagram-242.py

---
+++ array-valid-anagram-242.py
@@ -0,0 +1,40 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+"""
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        s_list = list(s)
+        t_list = list(t)
+        s_list.sort()
+        t_list.sort()
+    
+        if s_list == t_list:
+            return True
+        else: return False
+        
